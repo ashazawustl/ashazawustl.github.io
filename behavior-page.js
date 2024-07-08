1c17fcbc8bfb3951baa30d988bcfc27756de5f7a
salyaly2k3 <a.shaza@wustl.edu>
2024-07-08 16:26:19 -0500

    carousel behavior page

--- behavior-page.js
+++
@@ -1,7 +0,0 @@
-import Carousel from './carousel';
-
-const carouselControls = document.querySelector('#carousel-controls');
-const carousel = new Carousel({
-  root: document.querySelector('#carousel'),
-  navigationControls: carouselControls.content.cloneNode(true),
-});
